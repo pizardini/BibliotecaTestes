7e143877e2f3e212b87e4fa6625263d82f87d0ac
Pietro Zardini <pizardini@gmail.com>
2023-05-17 21:25:01 -0300

    adição do teste de listar tudo

--- src/test/java/ada/tech/Biblioteca/controller/LivroControllerTest.java
+++ src/test/java/ada/tech/Biblioteca/controller/LivroControllerTest.java
@@ -4,9 +4,11 @@ import ada.tech.Biblioteca.model.dto.LivroDTO;
 import ada.tech.Biblioteca.model.entity.LivroEntity;
 import ada.tech.Biblioteca.repository.LivroRepository;
 import ada.tech.Biblioteca.model.mapper.LivroMapper;
+import ada.tech.Biblioteca.service.LivroService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,13 +30,13 @@ public class LivroControllerTest {
     private LivroController livroController;
 
     @Autowired
-    private LivroMapper livroMapper;
-
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private LivroRepository repository;
+
+//    @Autowired
+//    private LivroService livroService;
 
     @Test
     public void carregouContexto() {
@@ -46,6 +50,30 @@ public class LivroControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    public void listarLivrosTest() throws Exception {
+        List<LivroDTO> listaLivros = new ArrayList<>(); // Lista simulada de livros
+
+
+        LivroDTO livro1 = new LivroDTO();
+        livro1.setTitulo("Livro Teste1");
+        livro1.setIsbn("1111111");
+        livro1.setResumo("Lorem ipsum");
+        livro1.setSumario("sumario de teste");
+        livro1.setPreco(59.90);
+        livro1.setPaginas(200);
+
+        listaLivros.add(livro1);
+
+        LivroService livroService = Mockito.mock(LivroService.class);
+        Mockito.when(livroService.listar()).thenReturn(listaLivros);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/livros"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
 
     @Test
     public void criarLivroTest() throws Exception {
